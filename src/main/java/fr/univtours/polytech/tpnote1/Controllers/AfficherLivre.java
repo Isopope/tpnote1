38edf2535f857eb0165890f2b246f0427c98ea0e
humbert nkouba <nkoubah@gmail.com>
2024-11-13 14:39:17 +0100

    creation des classes servlet et de la vue livres.jsp

---
+++ src/main/java/fr/univtours/polytech/tpnote1/Controllers/AfficherLivre.java
@@ -0,0 +1,22 @@
+package fr.univtours.polytech.tpnote1.Controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet(name="afficherLivre", urlPatterns = "/livres")
+public class AfficherLivre extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+
+}
