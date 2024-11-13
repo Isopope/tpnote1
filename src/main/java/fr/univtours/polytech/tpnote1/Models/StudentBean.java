ae30b0381941c72cff90e2774ffdf8897d7c5fd7
Chaimae-ch1 <chaaibi.chaimae1@gmail.com>
2024-11-13 13:05:15 +0100

    FIRST BEAN

---
+++ src/main/java/fr/univtours/polytech/tpnote1/Models/StudentBean.java
@@ -0,0 +1,50 @@
+package fr.univtours.polytech.tpnote1.Models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "STUDENT_JPA")
+public class StudentBean implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_STUDENT")
+    private Integer idStudent;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    public Integer getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(Integer idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+}
