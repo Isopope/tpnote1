bb177b3685bc3c1538df33d07a2892444e8c2b02
humbert nkouba <nkoubah@gmail.com>
2024-11-13 14:22:40 +0100

    modification de LivreDAO et livreDAOiMPL

--- src/main/java/fr/univtours/polytech/tpnote1/Daos/LivreDao.java
+++ src/main/java/fr/univtours/polytech/tpnote1/Daos/LivreDao.java
@@ -1,5 +1,11 @@
 package fr.univtours.polytech.tpnote1.Daos;
 
+import java.util.List;
+
+import fr.univtours.polytech.tpnote1.Models.LivreBean;
+
 public interface LivreDao {
+    public List<LivreBean> getAllLivre();
+    public LivreBean getOneLivre();
 
 }
--- src/main/java/fr/univtours/polytech/tpnote1/Daos/LivreDaoImpl.java
+++ src/main/java/fr/univtours/polytech/tpnote1/Daos/LivreDaoImpl.java
@@ -1,5 +1,28 @@
 package fr.univtours.polytech.tpnote1.Daos;
 
-public class LivreDaoImpl {
+import java.util.List;
+import fr.univtours.polytech.tpnote1.Models.LivreBean;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+@Stateless
+public class LivreDaoImpl implements LivreDao{
+    @PersistenceContext(unitName = "tp_note_1")
+    private EntityManager em;
+    @SuppressWarnings("unchecked")
+
+    @Override
+    public List<LivreBean> getAllLivre() {
+        Query req=em.createNativeQuery("SELECT * FROM BOOK", LivreBean.class);
+        return req.getResultList();
+        
+    }
+
+    @Override
+    public LivreBean getOneLivre() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOneLivre'");
+    }
 
 }
